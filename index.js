970d8b2f48353cc0f8334e6a34af66f633e72e0d
h4kst3r <ozil.hakim@gmail.com>
2019-10-28 16:08:07 +0100

    initial com

---
+++ index.js
@@ -0,0 +1,14 @@
+const navBar = document.querySelector('.header-topbar');
+
+window.addEventListener('scroll' ,  () => {
+    if(window.scrollY >= 200){
+
+        navBar.style.background = "white";
+        navBar.style.top = "0";
+        navBar.style.boxShadow =" 0 10px 10px rgba(34, 31, 31, 0.08)" 
+    } else {
+        navBar.style.background = "none";
+        navBar.style.top = "20px";
+        navBar.style.boxShadow ="none"
+    }
+})
